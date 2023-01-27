91b0cdedefc1d28564341057b1eb4a6ea0a189f3
stephenhawk8054 <nguyen_ftorres@yahoo.com>
2023-01-27 18:04:22 +0700

    generalize srcset

--- nojs_scriptlets.js
+++ nojs_scriptlets.js
@@ -1,6 +1,6 @@
 /// lazy-src.js
 (function() {
-    window.addEventListener("load", function() {
+    window.addEventListener("load", function () {
         for (const imgEl of document.querySelectorAll("img[data-lazy-src]")) {
             imgEl.src = imgEl.dataset.lazySrc;
         }
@@ -8,11 +8,13 @@
 })();
 
 
-/// lazy-van.js
+/// srcset.js
 (function() {
-    window.addEventListener("load", function() {
-        for (const imgEl of document.querySelectorAll("img.lazy-image-van")) {
-            let imgSplit = imgEl.dataset.srcset.split(",");
+    const target = "{{1}}";
+    const replace = "{{2}}";
+    window.addEventListener("load", function () {
+        for (const imgEl of document.querySelectorAll(target)) {
+            let imgSplit = imgEl.dataset[replace].split(",");
             imgEl.src = imgSplit[imgSplit.length - 1].trim().split(" ")[0];
         }
     });
@@ -21,13 +23,12 @@
 
 /// loading-lazy.js
 (function() {
-    window.addEventListener("load", function() {
+    window.addEventListener("load", function () {
         const selectorStr = '.right_container_flip img[loading="lazy"], #article_box img[loading="lazy"]';
-      
+
         for (let imgEl of document.querySelectorAll(selectorStr)) {
-          console.log(imgEl);
-          let imgSplit = imgEl.previousElementSibling.dataset.srcset.split(",");
-          imgEl.src = imgSplit[imgSplit.length - 1].trim().split(" ")[0];
+            let imgSplit = imgEl.previousElementSibling.dataset.srcset.split(",");
+            imgEl.src = imgSplit[imgSplit.length - 1].trim().split(" ")[0];
         }
     });
 })();
@@ -35,7 +36,7 @@
 
 /// picture-source.js
 (function() {
-    window.addEventListener("load", function() {
+    window.addEventListener("load", function () {
         for (let srcEl of document.querySelectorAll("picture > source")) {
             srcEl.srcset = srcEl.srcset.split("?")[0];
         }
@@ -45,10 +46,10 @@
 
 /// vanilla-gallery.js
 (function() {
-    window.addEventListener("load", function() {
+    window.addEventListener("load", function () {
         let scriptEl = document.querySelector('script[id^="vanilla-slice-imageGallery"]');
         let imgEl = document.querySelector("#article-body img.image-wrapped__image");
-      
+
         let dictStr = scriptEl.innerHTML.split("var data = ")[1].split(";\n")[0];
         imgEl.src = JSON.parse(dictStr).galleryData[0].image.src;
     });
