631c749277ace6972b8b476078735447e969275a
stephenhawk8054 <nguyen_ftorres@yahoo.com>
2023-01-28 12:39:21 +0700

    update bi-fix

--- nojs_scriptlets.js
+++ nojs_scriptlets.js
@@ -19,10 +19,12 @@
 
 /// bi-fix.js
 (function () {
-    for (const imgEl of document.querySelectorAll("img.lazy-image")) {
-        let imgJSON = JSON.parse(imgEl.dataset.srcs);
-        imgEl.src = Object.keys(imgJSON)[0];
-    }
+    window.addEventListener("load", function () {
+        for (const imgEl of document.querySelectorAll("img.lazy-image")) {
+            let imgJSON = JSON.parse(imgEl.dataset.srcs);
+            imgEl.src = Object.keys(imgJSON)[0];
+        }
+    });
 })();
 
 
