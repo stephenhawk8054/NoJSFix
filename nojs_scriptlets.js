97fb3a6b6244eb99a963a7f238f7eaae4054e874
stephenhawk8054 <nguyen_ftorres@yahoo.com>
2023-01-29 12:11:54 +0700

    update srcset regex

--- nojs_scriptlets.js
+++ nojs_scriptlets.js
@@ -7,7 +7,7 @@
     let attr = "src";
     if (attribute != "" && attribute != "{{3}}") { attr = attribute };
     // const regexp = /(?<=\d[a-z],)(?:.*?)(?= \d{1,4}w)|(?<=^)(?:.*?)(?= \d{1,4}[a-z])/g;
-    const regexp = /http\S+/g;
+    const regexp = /\S+\.[a-zA-Z]{3,4}/g;
     window.addEventListener("load", function () {
         for (const imgEl of document.querySelectorAll(target)) {
             let replaceData = imgEl;
