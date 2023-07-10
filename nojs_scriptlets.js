c6fed7497c0e21a2abf2dc80f13d42067ee97a31
stephenhawk8054 <nguyen_ftorres@yahoo.com>
2023-07-10 22:19:21 +0700

    update scriptlets

--- nojs_scriptlets.js
+++ nojs_scriptlets.js
@@ -74,39 +74,3 @@ function renameTag(
         }
     });
 }
-
-
-// /// bi-fix.js
-// (function () {
-//     window.addEventListener("load", function () {
-//         for (const imgEl of document.querySelectorAll("img.lazy-image[data-srcs]")) {
-//             let srcs = imgEl.dataset.srcs;
-//             if (srcs.startsWith("{")) {
-//                 let imgJSON = JSON.parse(srcs);
-//                 imgEl.src = Object.keys(imgJSON)[0];
-//             }
-//         }
-//     });
-// })();
-
-
-// /// vice-fix.js
-// (function () {
-//     window.addEventListener("load", function () {
-//         for (let srcEl of document.querySelectorAll("picture > source")) {
-//             srcEl.srcset = srcEl.srcset.split("?")[0];
-//         }
-//     });
-// })();
-
-
-// /// windowscentral-fix.js
-// (function () {
-//     window.addEventListener("load", function () {
-//         let scriptEl = document.querySelector('script[id^="vanilla-slice-imageGallery"]');
-//         let imgEl = document.querySelector("#article-body img.image-wrapped__image");
-
-//         let dictStr = scriptEl.innerHTML.split("var data = ")[1].split(";\n")[0];
-//         imgEl.src = JSON.parse(dictStr).galleryData[0].image.src;
-//     });
-// })();
