e7dab63bdc9352d5a5df2fda2e1f8905279d5991
stephenhawk8054 <nguyen_ftorres@yahoo.com>
2023-07-16 18:37:05 +0700

    update scriptlets

--- nojs_scriptlets.js
+++ nojs_scriptlets.js
@@ -74,3 +74,84 @@ function renameTag(
         }
     });
 }
+
+/// add-class.js
+/// alias ac.js
+/// world ISOLATED
+// example.com##+js(ac, class, [selector])
+function addClass(
+	needle = '',
+	selector = '' 
+) {
+	if ( needle === '' ) { return; }
+	const needles = needle.split(/\s*\|\s*/);
+	if ( selector === '' ) { selector = '.' + needles.map(a => CSS.escape(a)).join(',.'); }
+	const addclass = ev => {
+		if (ev) { self.removeEventListener(ev.type, addclass, true);  }
+		const nodes = document.querySelectorAll(selector);
+		try {
+			for ( const node of nodes ) {
+			      node.classList.add(...needles);
+			}
+		} catch { }
+	};
+	if (document.readyState === 'loading') {
+	      self.addEventListener('DOMContentLoaded', addclass, true);
+	} else {
+	      addclass();
+	}
+}
+
+/// replace-attr.js
+/// alias rpa.js
+/// world ISOLATED
+/// dependency run-at.fn
+// example.com##+js(rpa, [selector], oldattr, newattr, newvalue)
+function replaceAttr(
+	selector = '',
+	oldattr = '',
+	newattr = '',
+	value = '',
+	run = '' 
+) {
+	if ( selector === '' || oldattr === '' || newattr === '' ) { return; }
+	let timer;
+	const replaceattr = ( ) => {
+		timer = undefined;
+		const elems = document.querySelectorAll(selector);
+		try {
+			for ( const elem of elems ) {
+				if ( elem.hasAttribute( oldattr ) ) {
+				     elem.removeAttribute( oldattr );		
+				     elem.setAttribute( newattr, value );
+				}
+			}	
+		} catch { }
+	};
+	const mutationHandler = mutations => {
+		if ( timer !== undefined ) { return; }
+		let skip = true;
+		for ( let i = 0; i < mutations.length && skip; i++ ) {
+		    const { type, addedNodes, removedNodes } = mutations[i];
+		    if ( type === 'attributes' ) { skip = false; }
+		    for ( let j = 0; j < addedNodes.length && skip; j++ ) {
+			if ( addedNodes[j].nodeType === 1 ) { skip = false; break; }
+		    }
+		    for ( let j = 0; j < removedNodes.length && skip; j++ ) {
+			if ( removedNodes[j].nodeType === 1 ) { skip = false; break; }
+		    }
+		}
+		if ( skip ) { return; }
+		timer = self.requestAnimationFrame(replaceattr);
+	};
+	const start = ( ) => {
+		replaceattr();
+		if ( /\bloop\b/.test(run) === false ) { return; }
+		const observer = new MutationObserver(mutationHandler);
+		observer.observe(document.documentElement, {
+		    childList: true,
+		    subtree: true,
+		});
+	};
+	runAt(( ) => { start(); }, /\bcomplete\b/.test(run) ? 'idle' : 'interactive');
+}
