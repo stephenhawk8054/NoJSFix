9858d9fa8e466903ba3ef4674a1a084addf6b685
stephenhawk8054 <66517106+stephenhawk8054@users.noreply.github.com>
2025-09-03 23:58:32 +0700

    add userscript

---
+++ sandbox.user.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name        No JS
+// @namespace   User Scripts
+// @match       https://www.forbes.com/*
+// @grant       none
+// @run-at      document-end
+// @version     1.0
+// @author      -
+// @description Restore frames and images in no-scripting mode
+// ==/UserScript==
+
+// General -----------------------------------------------------------------
+const addNode = (nodeStr, source, parent) => {
+  if (source) {
+    let node = document.createElement(nodeStr);
+    node.src = source;
+    parent.appendChild(node);
+  }
+}
+
+// Forbes -----------------------------------------------------------------
+const forbes = () => {
+  document.querySelectorAll("fbs-embedly").forEach(fbsEmbed => {
+    addNode("iframe", fbsEmbed.attributes["iframe-src"]?.value, fbsEmbed)
+  });
+  document.querySelectorAll("progressive-image").forEach(imageProg => {
+    const imageSrc = imageProg.attributes["src"]?.value;
+    const imageBg = imageProg.attributes["background-image"]?.value;
+    if (imageSrc) { addNode("img", imageSrc, imageProg) }
+    else if (imageBg) { addNode("img", imageBg, imageProg) }
+  })
+}
+
+// Sites
+const hostname = location.hostname;
+if (hostname.includes("forbes.com")) { forbes() }
